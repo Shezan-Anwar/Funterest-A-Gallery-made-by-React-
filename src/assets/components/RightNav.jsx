041ea18804c92dd5db35106bfdf1d0c960edf614
Shezan-Anwar <anwarshezan96@gmail.com>
2026-06-05 17:53:32 +0530

    finished

--- src/assets/components/RightNav.jsx
+++ src/assets/components/RightNav.jsx
@@ -9,7 +9,7 @@ const RightNav = () => {
         CREATE <i class="ri-add-large-fill"></i>
       </div>
       <MessageSquareDot color="#000000" />
-      <div className='flex items-center gap-1'><img src="https://images.unsplash.com/photo-1549223565-49541e8416dc?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-10 h-10 rounded-full" />
+      <div className='flex items-center gap-1'><img src="https://images.unsplash.com/photo-1464746133101-a2c3f88e0dd9?q=80&w=2043&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-10 h-10 rounded-full" />
        <ChevronDown color="#000000" /></div>
     </div>
   )
