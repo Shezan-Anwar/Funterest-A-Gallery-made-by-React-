0282893ac6c546dadbb94658837229b75d0d4a9b
Shezan-Anwar <anwarshezan96@gmail.com>
2026-06-05 17:21:30 +0530

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './assets/components/Navbar'
 import axios from 'axios'
-async function getData(){
-      const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
-       console.log(response.data)
-       
-      }
+import {Bookmark, Scale} from 'lucide-react'
+import {MessageCircleHeart} from 'lucide-react'
+import { Loader } from 'lucide-react'
+
 const App = () => {
+  
+  const [index,setIndex] = useState(1);
+  const [userData, setUserData] = useState([]);
+  const getData = async () => {
+      const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=20`)
+      setUserData(response.data)
+      console.log(response.data)
+      console.log(index)
+      }
+  let printData = "Loading..."
+  if (userData.length > 0){
+    printData = userData.map(function(elem){
+      return <div key={elem.idx} className='h-77 w-63 bg-white rounded-2xl '>
+        <a href={elem.url} target='_blank' >
+        <img src={elem.download_url} alt="" className='w-full h-[85%] object-cover rounded-t-2xl' />
+        <div className='flex justify-between'>
+          <h2 className='font-bold  pl-2'>{elem.author}</h2>
+          <h4 className='pr-2 text-gray-700'>{elem.height}x{elem.width}</h4>
+          
+        </div>
+        </a>
+        <div className='flex flex-wrap justify-between gap-2 pb-1'>
+           <MessageCircleHeart color="#000000" /> <Bookmark color="#000000" />
+          </div>
+       
+      </div>
+    })
+  }
+  useEffect(function(){
+    getData()
+  },[index])
   return (
     <div>
       <Navbar />
-     <button onClick={getData}>Get Data</button>
-     <div>
-      
+     <div className='flex flex-wrap gap-6 w-screen  justify-center overflow-hidden  pt-22 '>
+      {printData}
+     </div>
+     <div className=' flex flex-row justify-center gap-5 m-10 '>
+      <button
+      style={{opacity : index==1 ? 0.5 : 1}}
+      onClick={()=>{
+        if (index > 1){
+        setIndex(index-1)
+        setUserData([])
+        }
+      }} 
+      className='bg-red-600 text-white font-semibold h-12 w-15 active:scale-95'>Prev</button>
+      <button 
+       onClick={()=>{
+        
+        setIndex(index+1)
+        setUserData([])
+       
+      }} 
+      className='bg-red-600 text-white font-semibold h-12 w-15'>Next</button>
      </div>
     </div>
   )
--- src/assets/components/RightNav.jsx
+++ src/assets/components/RightNav.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import 'remixicon/fonts/remixicon.css'
+import { ChevronDown } from 'lucide-react'
+import { MessageSquareDot } from 'lucide-react'
 const RightNav = () => {
   return (
     <div className="flex items-center gap-4">
       <div className="bg-black text-white p-2 rounded-md font-medium w-auto">
         CREATE <i class="ri-add-large-fill"></i>
       </div>
-      <i class="ri-chat-unread-line"></i>
-      <div className='flex items-center gap-1'><img src="https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-10 h-10 rounded-full" />
-        <i class="ri-arrow-down-s-line"></i></div>
+      <MessageSquareDot color="#000000" />
+      <div className='flex items-center gap-1'><img src="https://images.unsplash.com/photo-1549223565-49541e8416dc?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-10 h-10 rounded-full" />
+       <ChevronDown color="#000000" /></div>
     </div>
   )
 }
